749efcaa343a9ff3db17274632b3e5900a77b23d
Michael Perez <mperez980089@yahoo.com>
2019-09-28 15:22:59 -0700

    Setup blend for player idle>walk>run

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    public PlayerController playerController;
+    public Animator anim;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        anim.SetFloat("Speed", playerController.rb.velocity.magnitude);
+    }
+}
